40e8b53180abf0ce9fbe4f6885bead773505780b
hajar <hajarbenbella.8@gmail.com>
2026-01-28 14:20:20 +0100

    challenges1

--- week_1/day_2/daily_challenge/challenges.js
+++ week_1/day_2/daily_challenge/challenges.js
@@ -1,5 +1,21 @@
-// ====== Daily Challenge 1
+let planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
 
-// ====== Daily Challenge 2
+const section = document.querySelector(".listPlanets");
 
-// ====== Daily Challenge 3
+for (let i = 0; i < planets.length; i++) {
+    let planetDiv = document.createElement("div");
+    planetDiv.className = "planet";
+    planetDiv.textContent = planets[i];
+
+    switch (planets[i]) {
+        case "Mercury": planetDiv.style.backgroundColor = "gray"; break;
+        case "Venus": planetDiv.style.backgroundColor = "yellow"; planetDiv.style.color = "black"; break;
+        case "Earth": planetDiv.style.backgroundColor = "blue"; break;
+        case "Mars": planetDiv.style.backgroundColor = "red"; break;
+        case "Jupiter": planetDiv.style.backgroundColor = "orange"; break;
+        case "Saturn": planetDiv.style.backgroundColor = "lightyellow"; planetDiv.style.color = "black"; break;
+        case "Uranus": planetDiv.style.backgroundColor = "lightblue"; planetDiv.style.color = "black"; break;
+        case "Neptune": planetDiv.style.backgroundColor = "darkblue"; break;
+    }
+    section.appendChild(planetDiv);
+}
