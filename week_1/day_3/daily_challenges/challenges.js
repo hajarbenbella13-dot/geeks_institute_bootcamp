8e9ffbb90c491b8f55bcd0275fbc471c4fd1b13e
hajar <hajarbenbella.8@gmail.com>
2026-01-30 15:58:03 +0100

    challenge day3

--- week_1/day_3/daily_challenges/challenges.js
+++ week_1/day_3/daily_challenges/challenges.js
@@ -1,5 +1,30 @@
-// ====== Daily Challenge 1
+const inputNoun = document.getElementById("noun");
+const inputadjective = document.getElementById("adjective");
+const inputperson = document.getElementById("person");
+const inputverb = document.getElementById("verb");
+const inputplace = document.getElementById("place");  
+const story = document.getElementById("story");
+const form = document.getElementById("libform");
+form.addEventListener("submit", function (e) {
+ e.preventDefault();
+    if (
+        inputNoun.value === "" ||
+        inputadjective.value === "" ||
+        inputperson.value === "" ||
+        inputverb.value === "" ||
+        inputplace.value === ""
+    ) {
+        alert("Please fill in all the fields.");
+        return;
+    }
 
-// ====== Daily Challenge 2
+    story.textContent = `One day, ${inputperson.value} was walking through the ${inputplace.value}
+    when they saw a ${inputadjective.value} ${inputNoun.value}.
+    Excitedly, they decided to ${inputverb.value} it!`;
 
-// ====== Daily Challenge 3
+    inputnoun.value = "";
+    inputadjective.value = "";
+    inputperson.value = "";
+    inputverb.value = "";
+    inputplace.value = "";
+});
